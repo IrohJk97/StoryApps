8c4d3969c3daa09c2751ee55bd41f20bf1b01f02
MuzammilulMuttaqin <muzammilulmuttaqin11@gmail.com>
2022-12-02 11:06:53 +0700

    fix autDummy

--- app/src/test/java/com/musyarrofah/storyapps/utils/AuthDummy.kt
+++ app/src/test/java/com/musyarrofah/storyapps/utils/AuthDummy.kt
@@ -2,7 +2,6 @@ package com.musyarrofah.storyapps.utils
 
 import com.musyarrofah.storyapps.addstory.CreateStoryResponse
 import com.musyarrofah.storyapps.liststory.StoryResponse
-import com.musyarrofah.storyapps.liststory.UserModel
 import com.musyarrofah.storyapps.login.LoginResponse
 import com.musyarrofah.storyapps.login.LoginResult
 import com.musyarrofah.storyapps.register.RegisterResponse
@@ -15,7 +14,7 @@ object AuthDummy {
             LoginResult(
                 "user-XNkh2yhu1ETa8Wvt",
                 "siapa aja",
-                "token"
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLVhOa2gyeWh1MUVUYThXdnQiLCJpYXQiOjE2Njk5NTE5OTN9.80o9iE2PH3Yup4dIDz2YAlOmzwcZ30Ib9kUNGBXYxFM"
             )
         )
     }
@@ -27,34 +26,29 @@ object AuthDummy {
 
         for (i in 0 until 10) {
             val story = StoryResponse.Story(
-                "",
-                "",
-                "",
-                "",
-                ""
+                "user-XNkh2yhu1ETa8Wvt",
+                "siapa aja",
+                "cek lokasi",
+                "https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                "2022-11-02",
+                -6.1335033,
+                106.64356
             )
             item.add(story)
         }
         return item
     }
-
-    fun provideGetUser(): UserModel {
-        return UserModel(
-            "",
-            "token",
-            true
-        )
-    }
-
     fun provideDummyStoryLocation(): StoryResponse {
         val item: MutableList<StoryResponse.Story> = arrayListOf()
         for (i in 0..100) {
             val story = StoryResponse.Story(
-                "",
-                "",
-                "",
-                "",
-                ""
+                "user-XNkh2yhu1ETa8Wvt",
+                "siapa aja",
+                "cek lokasi",
+                "https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                "2022-11-02",
+                -6.1335033,
+                106.64356
             )
             item.add(story)
         }
